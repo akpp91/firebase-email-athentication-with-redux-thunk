874e2912a00bc11440bdce291c1728c02351446f
akpp91 <127812871+akpp91@users.noreply.github.com>
2023-11-28 07:46:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -130,13 +130,7 @@ const App = () => {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCW-q-XhnY7VJcijBTwNCsbmavCR6AWSqk",
-  authDomain: "react-native-course-99927.firebaseapp.com",
-  projectId: "react-native-course-99927",
-  storageBucket: "react-native-course-99927.appspot.com",
-  messagingSenderId: "86450240555",
-  appId: "1:86450240555:web:8aeb33a5192540a8a8fe6e",
-  measurementId: "G-WH5YYDQW2V"
+  apiKey: 
 };
 
 //Initialize Firebase
